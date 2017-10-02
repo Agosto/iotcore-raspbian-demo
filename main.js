9513c989206a4358cb18667d32b3738d84233a86
rpgobjects <rpgobjects@gmail.com>
2017-10-02 11:37:39 -0500

    handling blocked communications from iot core console.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const iotcore = require('./lib/iotcore');
 const provisioning = require('./lib/provisioning');
 const leds = require('./lib/leds');
 
+const PUBLISH_INTERVAL = 1000*10;
+
 /**
  * connects to IoT Core MQTT bridge, subscribes to the config topic, and publishes to the telemetry topic 1/min.
  * when token expires, recursively calls this function to reconnect and continue.
@@ -10,7 +12,7 @@ const leds = require('./lib/leds');
  */
 function startPublishing(settings) {
   console.log(settings);
-  return iotcore.connect(settings,provisioning.privateKeyFile)
+  return connectWithRetry(settings)
     .then(client=>{
       client.subscribe(iotcore.configTopic(settings.deviceId),{qos: 1});
       // listen for config changes
@@ -33,20 +35,48 @@ function startPublishing(settings) {
       const timerId = setInterval(()=>{
         client.publish(topic, `${settings.deviceId} ${new Date().toISOString()}`);
         leds.ledOn(0, 255, 0, 5000);
-      },(1000*60));
+      },PUBLISH_INTERVAL);
 
       // error handler for expired token
-      client.on('error', (err) => {
-        //console.log('error', err);
+      client.once('error', (err) => {
+        console.log('error', err);
         clearInterval(timerId);
         client.end(true,()=>{
-          console.log('token expired, reconnecting..');
-          // recursive callback
-          startPublishing(settings).catch(e=>console.warn(e));
+          // likely an expired token or block communications
+          // restart publishing
+          startPublishing(settings).catch(error=>console.warn('unrecoverable',error));
         })
       });
       return Promise.resolve();
+    });
+}
+
+/**
+ * simple timeout wrapped in a promise
+ * @param {int} delay ms to delay
+ * @param {object} [result] result to use on the resolve
+ * @returns {Promise}
+ */
+function delayedPromise(delay,result) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(result);
+    }, delay);
   });
+}
+
+/**
+ * Connects to ito core.  If an error occurs, will continue to retry every PUBLISH_INTERVAL until successful
+ * @param {DeviceSettings} settings
+ */
+function connectWithRetry(settings) {
+  return iotcore.connect(settings,provisioning.privateKeyFile)
+    .catch(error=>{
+      leds.ledOn(255, 0, 0, 5000);
+      console.warn('connect error',error);
+      console.log(`attempting reconnect in ${PUBLISH_INTERVAL}ms`);
+      return delayedPromise(PUBLISH_INTERVAL).then(()=>connectWithRetry(settings));
+    });
 }
 
 /**
@@ -56,13 +86,13 @@ function main() {
   leds.ledOn(255,0,0);
   // wait for provisioning
   provisioning.provision()
-    // connect to IoT core
-    .then(settings=>startPublishing(settings))
     // unrecoverable error
     .catch(error=>{
       leds.ledOn(255,0,0);
       console.warn(error);
-    });
+    })
+    // connect to IoT core
+    .then(settings=>startPublishing(settings))
 }
 
 main();
